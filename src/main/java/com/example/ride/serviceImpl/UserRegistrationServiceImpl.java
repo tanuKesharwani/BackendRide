7f6509d2059cfd7aa3c9f32230d53712a3607eb3
tanuKesharwani <tanu.kesharwani@gmail.com>
2024-12-21 11:44:30 +0530

    commit for changes in servcie impl

--- src/main/java/com/example/ride/serviceImpl/UserRegistrationServiceImpl.java
+++ src/main/java/com/example/ride/serviceImpl/UserRegistrationServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.ride.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.ride.Crud.BikeDetailsCrud;
+import com.example.ride.Entity.BikeDetails;
 import com.example.ride.Request.UserRegistrationRequest;
 import com.example.ride.pojo.GenericWebServiceResponse;
 import com.example.ride.service.UserRegistrationService;
@@ -9,8 +12,16 @@ import com.example.ride.service.UserRegistrationService;
 @Service
 public class UserRegistrationServiceImpl implements UserRegistrationService {
 
+	@Autowired
+	private BikeDetailsCrud bikeDetailsCrud;
+
 	@Override
 	public GenericWebServiceResponse saveUserDetails(UserRegistrationRequest request) {
+
+		BikeDetails bikeRide = new BikeDetails();
+		bikeRide.setBikeCc("200");
+		bikeRide.setBikeModel("bullet");
+		bikeDetailsCrud.save(bikeRide);
 		return null;
 	}
 
